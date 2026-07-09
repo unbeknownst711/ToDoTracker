9910ad19f278d979c3b0c97046d551325f37f3ad
unbeknownst711 <teswayae@gmail.com>
2026-07-09 14:51:42 +0700

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+//======    Config  =============
+const DEBUG_INFO = 1.0
+
+
+
+//======    Var     =============
+var ItemArr = {}
+
+let index = 0
+
+
+
+//======    Init    =============
+
+function Init(){
+
+}
+
+
+
+
+//======    Debug   ==============
+
+console.log("Debug Ver: " + DEBUG_INFO)
